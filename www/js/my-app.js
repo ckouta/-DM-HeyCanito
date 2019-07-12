03034d1e1b4dad52073b551a78aa526fdaf443c3
ckouta <ckouta25@gmail.com>
2019-07-12 15:53:51 -0400

    cc

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -6,6 +6,8 @@ var myApp = new Framework7();
 // If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
 
+var bolsa = [];
+
 var app = new Framework7({
   // App root element
   root: '#app',
@@ -61,12 +63,6 @@ $$(document).on('page:init', function (e) {
   // Do something here when page loaded and initialized
   app.request.get('http://localhost:3005/productos',{}, function (data) {
    // var html='<p>'+data.data.nombre+'</p>';
-    var td =[];
-    var valores = JSON.parse(data);
-    valores.data.forEach(element => {
-      td.push('<tr><td>'+element.nombre+'</td><td>'+element.precio+'</td><td>'+element.descripcion+'</td><td>'+element[3]+'</td><td>'+element[4]+'</td><td>'+element[5]+'</td></tr>');
-    });
-    $$('#articles').html(td);
  
 });
   console.log(e);
@@ -145,7 +141,7 @@ function myFunction(id) {
 function detalle(id) {
   var td =[];
   app.request.get('http://localhost:3005/producto/'+id,{}, function (data) {
-    console.log("paso aqui");
+    console.log("ya no esta pasando por aqui");
     var valores = JSON.parse(data);
     valores.data.forEach(element => {
       td.push('<h1>'+element.nombre+'</h1>');
@@ -165,7 +161,7 @@ function detalle(id) {
       td.push('          </div>');
       td.push('     </div>');
       td.push('     <div class="col">');
-      td.push('         <button class="button col">Carrito</button>');
+      td.push('         <button class="col button button-fill color-orange">Carrito ver</button>');
       td.push('     </div>');
       td.push('  </div>');
     });
@@ -198,3 +194,19 @@ $$('.login-screen').on('loginscreen:close', function (e, loginScreen) {
 $$('.login-screen').on('loginscreen:closed', function (e, loginScreen) {
   console.log('Login screen closed')
 });
+
+/* <div class="data-table card">
+            <table>
+              <thead>
+                <th class="label-cell">nombre</th>
+                <th class="label-cell">precio</th>
+                <th class="label-cell">Descripcion</th>
+                <th class="label-cell">categoria</th>
+                <th class="label-cell">imagen</th>
+                <th class="label-cell">cantidad de personas</th>
+              </thead>
+              <tbody id="articles">
+              
+              </tbody>
+            </table>
+          </div>*/
